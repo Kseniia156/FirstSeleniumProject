1e78956c62627f8c844f16d84418b1ccaa42e013
Kseniia156 <hedyrover@gmail.com>
2024-11-19 23:55:34 +0100

    0

--- src/test/java/com/ait/qa49/FindElementTests.java
+++ src/test/java/com/ait/qa49/FindElementTests.java
@@ -43,29 +43,43 @@ public class FindElementTests {
     public void findElementBySimpleLocators() {
 
         driver.findElement(By.id("city"));
-
         driver.findElement(By.className("header"));
-
         driver.findElement(By.linkText("Let the car work"));
-
         driver.findElement(By.partialLinkText("car"));
     }
     @Test
     public void findElementByCssSelectorTest() {
 
         driver.findElement(By.cssSelector("h1"));
-
         driver.findElement(By.cssSelector("#city"));
-
         driver.findElement(By.cssSelector(".header"));
-
         driver.findElement(By.cssSelector("[href='/login?url=%2Fsearch']"));
-
         driver.findElement(By.cssSelector("[href*='login']"));
-
         driver.findElement(By.cssSelector("[href^='/log']"));
-
         driver.findElement(By.cssSelector("[href$='search']"));
+        driver.findElement(By.cssSelector("input#city"));
+        driver.findElement(By.cssSelector("div.search-card"));
+                driver.findElement(By.cssSelector("input#city[type='text']"));
+    }
+    @Test
+    public void findElementByXpath() {
+
+        driver.findElement(By.xpath("//h1"));
+        driver.findElement(By.xpath("//input[@id='city']"));
+        driver.findElement(By.xpath("//div[@class='header']"));
+        driver.findElement(By.xpath("//h2[contains(.,'Yalla')]"));
+        driver.findElement(By.xpath("//h2[text()='Type your data and hit Yalla!']"));
+        driver.findElement(By.xpath("//h2[.='Type your data and hit Yalla!']"));
+        driver.findElement(By.xpath("//label[starts-with(@for,'ci')]"));
+        driver.findElement(By.xpath("//a[@class='navigation-link']/.."));
+        driver.findElement(By.xpath("//h1/parent::*"));
+        driver.findElement(By.xpath("//h1/parent::div"));
+        driver.findElement(By.xpath("//h1/.."));
+        driver.findElement(By.xpath("//h1/ancestor::*"));//all
+        driver.findElement(By.xpath("//h1/ancestor::div"));//two options
+        driver.findElement(By.xpath("//h1/ancestor::div[2]"));//one option
+        driver.findElement(By.xpath("//h1/following-sibling::form"));
+        driver.findElement(By.xpath("//h2/preceding-sibling::*"));
     }
 
 }
